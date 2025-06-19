5907ca872dfa17f084fcc555ae41d041e84e1fd7
Davi <davif127@gmail.com>
2025-06-19 02:13:23 -0300

    Update api_config.dart

--- frontend/lib/config/api_config.dart
+++ frontend/lib/config/api_config.dart
@@ -2,7 +2,7 @@ class ApiConfig {
   static const String googleMapsApiKey = '';
   
   static const String authBaseUrl = 'http://localhost:8080';
-  static const String pedidosBaseUrl = 'http://localhost:8084';
+  static const String pedidosBaseUrl = 'http://localhost:8080';
   
   static const String authApiUrl = '$authBaseUrl/api/auth';
   static const String pedidosApiUrl = '$pedidosBaseUrl/api/pedidos';
